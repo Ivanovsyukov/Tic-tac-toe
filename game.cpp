5464197ee5a5525599524e2e12d5404775f692e8
Ivan Ovsyukov <st117301@student.spbu.ru>
2024-10-21 01:42:40 +0300

    Добавление кода для ввода класса, его удаления, выбор первого игрока, проверка победы, шаг.

--- game.cpp
+++ game.cpp
@@ -1,5 +1,70 @@
-#include <iostream>
+#include "game.h"
 
-int main(){
-    return 0;
+int Tic_tac_toe::count_char_on_line_(int row, int col, char search, int row_change, int col_change){
+    int count=0;
+    bool end=false;
+    for(int i=row; !end && i>=0 && i<=row; i+=row_change){
+        for(int j=col; !end && j>=0 && j<=col; j+=col_change){
+            if(game_space_[i][j]==search){
+                ++count;
+            } else {
+                end=true;
+            }
+        }
+    }
+    return count;
+}
+
+bool Tic_tac_toe::check_win(int row, int col, char win){
+    int count=0;
+    for(int i=0; i<=1; ++i){
+        for(int j=0; j<=1; ++j){
+            if(i==j && i==0){
+                count=count_char_on_line_(row, col, win, -1, 1)+count_char_on_line_(row, col, win, 1, -1)-1;
+            } else {
+                count=count_char_on_line_(row, col, win, i, j)+count_char_on_line_(row, col, win, -i, -j)-1;
+            }
+            if(count>=len_win_line_){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+bool Tic_tac_toe::first_player_choose(){
+    return bool((std::rand())%2);
+}
+
+Tic_tac_toe::Tic_tac_toe(int row, int col, int len_win_line, std::string player_name_1, std::string player_name_2){
+    row_=row;
+    col_=col;
+    len_win_line_=len_win_line;
+    player_name_1_=player_name_1;
+    player_name_2_=player_name_2;
+    game_space_=new char*[row];
+    for(size_t k=0; k<col; ++k){
+        game_space_[k]=new char[col];
+        for(size_t p=0; p<col; ++p){
+            game_space_[k][p]=none_;
+        }
+    }
+}
+
+int Tic_tac_toe::step(int row, int col, char player_char){
+    if(game_space_[row][col]!=none_){
+        return 0;//занятое поле
+    }
+    game_space_[row][col]=player_char;
+    if(check_win(row, col, player_char)){
+        return 2;//победа
+    }
+    return 1;//ход проведен успешно
+}
+
+Tic_tac_toe::~Tic_tac_toe(){
+    for(size_t k=0; k<col_; ++k){
+        delete[] game_space_[k];
+    }
+    delete[] game_space_;
 }
--- game.h
+++ game.h
@@ -1,17 +1,28 @@
 #ifndef GAME_2024
 #define GAME_2024
 
-class Tic_tac_toe{
-    unsigned int row;
-    unsigned int col;
-    unsigned int** game_space;
-    unsigned int len_win_line;
+#include <string>
 
-    unsigned int first_player_choose();
-    bool check_win();
+class Tic_tac_toe{
+    char one_char_='X';
+    char two_char_='O';
+    char none_='_';
+    int row_;
+    int col_;
+    char** game_space_;
+    int len_win_line_;
+    std::string player_name_1_;
+    std::string player_name_2_;
+    bool first_step_;
+
+    bool first_player_choose();//0 - первый, 1 - второй.
+    bool check_win(int row, int col, char win);
+    int count_char_on_line_(int row, int col, char search, int row_change, int col_change);
 public:
-    Tic_tac_toe(unsigned int row, unsigned int col, unsigned int len_win_line);
+    Tic_tac_toe(int row=3, int col=3, int len_win_line=3, std::string player_name_1="1", std::string player_name_2="2");
+    std::string out_place();
+    int step(int row, int col, char player_char);
     ~Tic_tac_toe();
-}
+};
 
 #endif
